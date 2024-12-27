c517323d6e3369cde194ddfcb49a9c7147cbf69b
MK <newlink05@mail.ru>
2024-12-27 09:47:44 +0500

    add JUnit4 tests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,24 +1,23 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
-
-import static org.testng.Assert.*;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class CashbackHackServiceTest {
 
     @Test
     public void shouldRemainBeforeBoundary() {
         CashbackHackService service = new CashbackHackService();
-        assertEquals(service.remain(9999), 1);
+        Assert.assertEquals(1, service.remain(9999));
     }
     @Test
     public void shouldNoRemainBoundary() {
         CashbackHackService service = new CashbackHackService();
-        assertEquals(service.remain(10000), 0);
+        Assert.assertEquals(0, service.remain(10000));
     }
     @Test
     public void shouldNoRemainAfterBoundary() {
         CashbackHackService service = new CashbackHackService();
-        assertEquals(service.remain(10001), 999);
+        Assert.assertEquals(999, service.remain(10001));
     }
 }
